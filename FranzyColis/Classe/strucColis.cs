e0869a81f8deeeb284cad0901c7e296620fa85fa
Guapito76 <romaind.cci@gmail.com>
2014-03-03 14:41:49 +0100

    Erreur2

---
+++ FranzyColis/Classe/strucColis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Franzy_Colis.Objet.Colis
+{
+    public struct ColisRecu
+    {
+        public int id;
+        public string Destinataire;
+        public string Adresse;
+        public double X;
+        public double Y;
+    }
+
+    public struct ColisEnvoyer
+    {
+        int id;
+        int Etat;
+    }
+}
--- FranzyColis/frmLivraison.xaml.cs
+++ FranzyColis/frmLivraison.xaml.cs
@@ -43,8 +43,12 @@ namespace FranzyColis
             lati = args.Position.Coordinate.Latitude;
             longi = args.Position.Coordinate.Longitude;
             var truc = args.Position.Coordinate;
+<<<<<<< HEAD
             MyMap.SetView(truc, 0.5);
 
+=======
+            MyMap.SetView(new System.Device.Location.GeoCoordinate(lati, longi), 1);
+>>>>>>> 294d06321b262bf243c49decb45588a3b1ff8b08
             //MyMap.Center = truc;
             
         } 
